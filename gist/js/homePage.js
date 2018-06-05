8311d5bc626e42df2fad2ec9a89ef100d3a7627e
Arthur James <11641990+ArtiDjeims@users.noreply.github.com>
2018-06-05 22:21:31 +0300

    added about us button

---
+++ gist/js/homePage.js
@@ -0,0 +1,23 @@
+function run() {
+    let gist = document.getElementById("gist").value;
+    let file = document.getElementById("file").value;
+    window.location = `gist/sandbox/index.html?gist=${gist}&file=${file}`;
+}
+
+function openLastGist() {
+    let lastGist = localStorage.getItem('gist');
+    let lastFile = localStorage.getItem('file');
+    if (lastGist && lastFile) {
+        window.location = `gist/sandbox/index.html?gist=${lastGist}&file=${lastFile}`;
+    } else {
+        alert("You haven't open any Gists lately!")
+    }
+}
+
+function createNewGist() {
+    window.open('https://gist.github.com/', '_blank');
+}
+
+function aboutGistFiddle() {
+    window.open('https://github.com/ArtiDjeims/GistFiddle/blob/master/README.md', '_blank');
+}
